eb20d868bb8b99ebafa5e4c96dd0862665929548
Tucker Elliott <120062499+tuckerelliott@users.noreply.github.com>
2024-05-03 13:01:56 -0400

    Update offer.js

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
-  const aempublishurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
-  const aemauthorurl = 'https://author-p123917-e1220159.adobeaemcloud.com';
+  const aempublishurl = 'https://publish-p99452-e916802.adobeaemcloud.com';
+  const aemauthorurl = 'https://author-p99452-e916802.adobeaemcloud.com';
   const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
   const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
