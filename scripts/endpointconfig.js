cebd9046e2d0de0afd67223786ec71e93dcf6ff0
Tucker Elliott <120062499+tuckerelliott@users.noreply.github.com>
2025-08-28 14:49:09 -0400

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p134711-e1334063.adobeaemcloud.com/';
+  return 'https://publish-p134711-e1334063.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p134711-e1334063.adobeaemcloud.com/';
+  return 'https://author-p134711-e1334063.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
